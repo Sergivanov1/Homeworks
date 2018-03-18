2057b75feecaed1230060bee09866a370e32bcd1
Sergivanov1 <ise200017@gmail.com>
2018-03-18 19:35:34 +0300

    Final
    ready

--- main2.py
+++ main2.py
@@ -21,16 +21,26 @@ def callback(event):
                 else:
                     continue
     count = 0
+    stepeni = []
     for i in range(6):
+        stepen = 0
         for j in range(6):
             print(matrix[i][j], end='')
             print(' ', end = '')
             if matrix[i][j] == 1:
                 count += 1
+                stepen += 1
+        stepeni.append(stepen)
         print('')
     print('Кол-во единиц: ', count)
     print('Вероятность единицы: ', count/36)
-    canvas.bind("<Button-1>", callback)
+    l = []
+    for i in range(len(stepeni)):
+        if stepeni[i] == max(stepeni):
+            l.append(i + 1)
+    print("Наибольшая степень: ", max(stepeni))
+    print("Вершины наибольшей степени: ", end='')
+    print(l)
     #---------------CIRCLES---------------#
     oval1 = canvas.create_oval(3*point, point, 5*point, 3*point,
                                outline="black", fill="blue", width=0)
@@ -44,9 +54,18 @@ def callback(event):
                                outline="black", fill="blue", width=0)
     oval6 = canvas.create_oval(point, 5*point, 3*point, 7*point,
                                outline="black", fill="blue", width=0)
-    # -------------------------------------#
+    #-------------------------------------#
+    #-----------------TEXT-----------------#
+    tx1 = canvas.create_text(150, 50, width=25, font=("Arial", 35), text="1")
+    tx2 = canvas.create_text(450, 50, width=25, font=("Arial", 35), text="2")
+    tx3 = canvas.create_text(550, 250, width=25, font=("Arial", 35), text="3")
+    tx4 = canvas.create_text(450, 450, width=25, font=("Arial", 35), text="4")
+    tx5 = canvas.create_text(150, 450, width=25, font=("Arial", 35), text="5")
+    tx6 = canvas.create_text(50, 250, width=25, font=("Arial", 35), text="6")
+    #-----------------TEXT-----------------#
     centers = [[200, 100], [400,100], [500, 300], [400, 500], [200, 500], [100, 300]]
     l = 0
+
     for i in range(6):
         for j in range(6):
             if matrix[i][j] == 1:
@@ -55,5 +74,3 @@ def callback(event):
                                           width = 3, fill="red")
 canvas.bind("<Button-1>", callback)
 mainloop()
-
-
